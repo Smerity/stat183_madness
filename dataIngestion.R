aa4cea6c84877f050fa211b02a62d31b0bb6fbfd
Kevin Kuate Fodouop <kevin.kuate@gmail.com>
2014-02-27 18:43:55 -0500

    Added data to the repo

--- dataIngestion.R
+++ dataIngestion.R
@@ -1,1 +1,11 @@
 ######### Handles data ingestion and translation into standard format #############
+
+# Set source location
+setwd("~/Documents/Harvard/Stats/Stat 183/March Madness/stat183_madness")
+
+parseTeam <- function(teamsFile, seedsFile, tourneyResultsFile, seasonResultsFile) {
+  teams <- read.csv(paste("./data",teamsFile, sep="/"))
+  tourney.seeds <- read.csv(paste("./data",seedsFile, sep="/"))
+  tourney.results <- read.csv(paste("./data",tourneyResultsFile, sep="/"))
+  season.results <- read.csv(paste("./data",seasonResultsFile, sep="/"))
+}
